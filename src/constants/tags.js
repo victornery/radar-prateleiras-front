0b45976848de4ec8dd96dc839fa88843f9df7e05
Victor Nery <victornery@MacBook-Pro-de-Victor.local>
2020-03-25 02:02:05 -0300

    Alterando espaçamento e editando autor

--- src/constants/tags.js
+++ src/constants/tags.js
@@ -1,13 +1,12 @@
 // GA TAGS ID
 export const GA_TRACKING_ID = 'UA-161417080-1'
 
-
 // for meta tag <og & twitter>
 export const SITE_NAME = 'Radar Prateleiras'
 export const SITE_TITLE = 'Radar Prateleiras'
 export const SITE_DESCRIPTION = 'Fazendo uma ponte do povo para as prateleiras.'
 export const SITE_IMAGE = ''
-export const SITE_AUTHOR = '@victornery'
+export const SITE_AUTHOR = '@radarprateleiras'
 export const SITE_KEYWORDS = ''
 export const SITE_URL = 'https://radarprateleiras.com.br'
 export const TWITTER_HANDLE = ''
